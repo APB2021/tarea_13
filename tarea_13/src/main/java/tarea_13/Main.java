dba6bc3bccb57f3189df0912adb518044c384e51
beton <beton@i7-6700K-Betonick>
2024-11-19 11:59:45 +0100

    Primer commit con la estructura de la tarea_13

---
+++ tarea_13/src/main/java/tarea_13/Main.java
@@ -0,0 +1,7 @@
+package tarea_13;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tarea_13/src/test/java/tarea_13/tarea_13/AppTest.java
@@ -0,0 +1,19 @@
+package tarea_13.tarea_13;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
